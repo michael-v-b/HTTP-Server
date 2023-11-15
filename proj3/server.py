2b40f82be312d48649c36a39a19603c3d464eba6
Noam Arana <noamarana@gmail.com>
2023-11-14 21:00:20 -0500

    Update server.py
    fixed modules

--- proj3/server.py
+++ proj3/server.py
@@ -4,25 +4,54 @@ import random
 import sys
 
 
-def main(accounts):
+def main():
+
+    # Checks for the correct number of command-line arguments
+    if len(sys.argv) != 6:
+        print("Usage: python3 server.py [IP] [PORT] [ACCOUNTS_FILE] [SESSION_TIMEOUT] [ROOT_DIRECTORY]")
+        sys.exit(1)
+
+    # Parse command-line arguments
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    accounts_file = sys.argv[3]
+    session_timeout = sys.argv[4]
+    root_directory = sys.argv[5]
+
+    with open('accounts_file', 'r') as f:
+        accounts = json.load(f)
+
      # Get the username and password from the user.
-    username = inpunt("Username: ")
-    password = inpunt("Password: ")
+    username = input("Username: ")
+    password = input("Password: ")
     message = login_request(username, password, accounts)
-    print message
+    print (message)
+    return message
 
 #ALERT ALERT ALERT NOAM ARANA IS GAY, REPEAT NOAM ARANA IS GAY!!!!
 
-def login_request(username, password, accounts):
+def http_request():
+    lines = http_request.split("\r\n")
+    lines = lines[1:] #ignore the GET / HTTP/1.1
+    output = {}
+    for line in lines:
+        if not line:
+            continue
+        key,value = line.split(':', 1)
+        output[key] = value   
+    print(output)
+    return output
+
+def login_request(username, password, accounts_file):
    
     # Validates "username" and "password" from headers, return 501 and log "LOGIN FAILED" if missing.
     if not username or not password:
-        message = (501, 'LOGIN FAILED')
+        message = ('LOGIN FAILED')
         print(message)
-        return message
+        return "HTTP/1.1 501 Not Implemented\r\n\r\n"
     
     # Create a session with the given credentials. 
-    valid_login_credentials = handle_login_credentials(username, password)
+    valid_login_credentials = handle_login_credentials(username, password, accounts_file)
 
     # Validates creds, sets 64-bit hex sessionID cookie, create & log session, return HTTP 200 with "Logged in!"
     if valid_login_credentials:
@@ -30,35 +59,24 @@ def login_request(username, password, accounts):
         message = (f'LOGIN SUCCESSFUL: {username} : {password}')
         print(message)
 
-        #Set the cookie and the status code with the message
-        # response = requests.Response()
-        # logging.info(f'LOGIN SUCCESSFUL: {username} : {password}')
-        # response.set_cookie("session_id", session_id)
-        # response.status_code = 200
-        # response.content('Logged in!')
-
-        return message
+        # Return the cookie and the status code with the message
+        return f"HTTP/1.1 200 OK\r\nSet-Cookie: sessionID={session_id}\r\n\r\nLogged in!"
     else:
         message = (f'LOGIN FAILED: {username} : {password}')
 
-        #Set the status code with the message
-        # response = requests.Response()
-        # logging.error(f'LOGIN FAILED: {username} : {password}')
-        # response.content('Login failed!')
-        # response.status_code = 200
-        return message
-
+        # Return the status code with the message
+        return "HTTP/1.1 200 OK\r\n\r\nLogin failed!"
 
 #This function handles the login credentials for a given user. It returns True if the credentials are correct and False otherwise.    
-def handle_login_credentials():
+def handle_login_credentials(username, password, accounts):
 
-    with open('accounts, 'r') as f:
-        accounts = json.load(f)
+    # Retrieve user info from accounts
+    if user_info:
+        hashed_password = hashlib.sha256((password + user_info['salt']).encode()).hexdigest()
+        hashed_password = user_info['password']
+        user_info = accounts.get(username)
+        return user_info
+    return False
 
-    hashed_password = hashlib.sha256((password + salt).encode()).hexdigest()
-    accounts.get(username) == hashed_password
-    return accounts
-
-if __main__ == "__main__":
+if __name__ == "__main__":
     main()
-        
